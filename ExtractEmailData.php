2b23af754614c3198f4d3546bc73516ebfda5494
cvramanarao <cvramanarao@gmail.com>
2015-10-05 22:18:41 +0530

    committing changes to extract voucher number

---
+++ ExtractEmailData.php
@@ -0,0 +1,24 @@
+<?php
+
+	
+    
+	function extractDataFromHtml($html){
+		$doc = new DOMDocument();
+		$doc->loadHTML($html);
+		$xpath = new DOMXPath($doc);
+		$tables = $doc->getElementsByTagName('table');
+		$nodes  = $xpath->query('.//tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td[2]', $tables->item(0));
+		//print_r($nodes);
+		//var_dump($nodes->item(0)->nodeValue.trim());
+		$voucherString = ''.$nodes->item(0)->nodeValue;
+		$voucherString = trim($voucherString);
+		$voucherNumber = substr($voucherString, strpos($voucherString, ":")+1);
+		print('Voucher Number is '.$voucherNumber." done");
+		
+	}
+		
+		libxml_use_internal_errors(false);
+	$link = "output.html";
+	$html = file_get_contents($link);
+	extractDataFromHtml($html);
+?>
