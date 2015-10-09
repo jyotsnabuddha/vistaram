4fe78b4b263b7f2b1a669d7c9dd81e8248313c1f
Venkat Chadaram <venkatchadaram@Venkats-iMac.Home>
2015-10-09 22:11:28 +0530

    Commit for data extraction

--- ExtractEmailData.php
+++ ExtractEmailData.php
@@ -6,53 +6,40 @@
 		$doc = new DOMDocument();
 		$doc->loadHTML($html);
 		$xpath = new DOMXPath($doc);
+
+		//Extract voucher number
 		$tables = $doc->getElementsByTagName('table');
 		$nodes  = $xpath->query('.//tbody/tr/td', $tables->item(0));
 		$voucherString = ''.$nodes->item(0)->nodeValue;
 		$voucherString = trim($voucherString);
 		$voucherNumber = substr($voucherString, strpos($voucherString, ":")+1);
 		print('Voucher Number is '.$voucherNumber."\n");
-		//return new VoucherDetails($voucherNumber);
-
-	
-	/*$xpath2 = new DOMXPath($doc);
-	
-	$body = $doc->getElementsByTagName('body');
-
-	$tdNodes = $xpath2->query('.//td', $body->item(0));
-
-	foreach($tdNodes as $tdNode){
-		$text = trim($tdNode->nodeValue);
-	
-		echo "\n\n".$text."\n\n\n";
-
-	}
-	
-	$nameString = ''.$nodes->item(0)->nodeValue;
-	$nameString = trim($nameString);
-	$guestName = substr($nameString , strpos($nameString , ":")+1);
-	print('Guest name is : '.$guestName."\n");
 		
+		//Extract voucher details
+			
+		$body = $doc->getElementsByTagName('body');
+		$i = 1;
+		$details = array();
+		while(true){
+			$nodes = $xpath->query('//body/table[3]/*/*/td/table/tbody/tr/td/table/tr['.$i.']/td[1]', $doc);
+			if($nodes->length == 0) break;
+			$key = trim($nodes->item(0)->nodeValue);
+			echo  "\n\nkey : ".$key."\n\n";
+			$nodes = $xpath->query('//body/table[3]/*/*/td/table/tbody/tr/td/table/tr['.$i.']/td[2]', $doc);
+			$value= trim($nodes->item(0)->nodeValue);
+			echo  "\n\nvalue : ".$value."\n\n";
+			$details[$key] = $value;
+			$i++;
+		} 
 
+		//Extract Bank payment details
 
-	$xpath->query('.//table[2]/tbody[1]/tr/td/table/tbody/table/tr[3]/td[2]', $tables)->$item[2];
-	$bookingstring = ''.$nodes->$item[2]->nodeValue;
-	$bookingDate = substr($bookingstring , strpos($bookingString,":")+1);
-	print('Booking date is '.$bookingDate."\n");
+		//Extract Tariff Applicable
 
-
-	$xpath->query('.//table[2]/tbody[1]/tr/td/table/tbody/table/tr[4]/td[2]', $tables)->$item[3];
-	$checkinstring = ''.$nodes->$item[2]->nodeValue;
-	$checkinDate = substr($checkinstring , strpos($checkinString,":")+1);
-	print('Checkin date is '.$checkinDate."\n");
-
-
-	$xpath->query('.//table[2]/tbody[1]/tr/td/table/tbody/table/tr[5]/td[2]', $tables)->$item[4];
-	$checkoutstring = ''.$nodes->$item[2]->nodeValue;
-	$checkoutDate = substr($checkoutstring , strpos($checkoutString,":"));
-	print('Checkout date is '.$checkoutDate."\n"); */
-
-		return new VoucherDetails($voucherNumber);
+		//Extract Summary
+		
+		var_dump($details);
+		return new VoucherDetails($voucherNumber,  $details["name of guest"]);
 	}
 		
 
